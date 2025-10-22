22e1d0647052a80112c7e69c5292ac8b6212f0b6
Dhathri33 <dbathini@luc.edu>
2025-10-22 14:05:03 -0500

    Initial Commit

---
+++ Pisacco-SwiftUI/Pisacco_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pisacco_SwiftUIApp.swift
+//  Pisacco-SwiftUI
+//
+//  Created by Dhathri Bathini on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Pisacco_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
